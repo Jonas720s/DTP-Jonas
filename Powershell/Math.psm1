2064bc86a32ebdd807844d94f5e7d22d5d4d07d7
Jonas720s <jonas.gemeinder@bluewin.ch>
2024-01-24 13:52:01 +0100

    Module kommentiert

--- Powershell/Math.psm1
+++ Powershell/Math.psm1
@@ -1,11 +1,21 @@
+
+<#.DESCRIPTION
+    adds two numbers
+#>
 function Sum($a, $b) {
     return "$a + $b = " + ($a + $b)
 }
 
+<#.DESCRIPTION
+    subtracts two numbers
+#>
 function Sub($a, $b) {
     return "$a - $b = " + ($a - $b)
 }
 
+<#.DESCRIPTION
+    divides two numbers
+#>
 function Div($a, $b) {
     if ($b -eq 0) {
         return "Error: Division by zero"
@@ -14,6 +24,9 @@ function Div($a, $b) {
     }
 }
 
+<#.DESCRIPTION
+    multiplies two numbers
+#>
 function Mul($a, $b) {
     return "$a * $b = " + ($a * $b)
 }
